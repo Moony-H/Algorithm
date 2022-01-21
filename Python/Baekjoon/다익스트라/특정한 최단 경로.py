d14f8ddbcbb8c9a89b42fe1fc3b8d313c1831292
Moony-H <moonylight247@gmail.com>
2022-01-21 23:55:33 +0900

    dijk

---
+++ Python/Baekjoon/다익스트라/특정한 최단 경로.py
@@ -0,0 +1,32 @@
+import heapq
+
+
+INF = int(1e9)
+n, e = map(int, input().split())
+
+table = [[]for _ in range(n)]
+
+
+for i in range(e):
+    a, b, cost = map(int, input().split())
+    table[a-1].append((b-1, cost))
+    table[b-1].append((a-1, cost))
+
+haveToA, haveToB = map(int, input().split())
+
+
+def dijkstra(start):
+    q = []
+    distance = [INF]*n
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in table[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+    return distance

cfa40351ec06785bcf5eb416a53f29c3a6ff9bb2
Moony-H <moonylight247@gmail.com>
2022-03-02 00:00:13 +0900

    oper

---
+++ Python/Baekjoon/완전 탐색/연산자 끼워넣기.py
@@ -0,0 +1,38 @@
+import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+maximum = -int(1e9)
+minimum = int(1e9)
+n = 0
+
+
+def dfs(p, m, mul, d, result, arr, depth):
+    global maximum
+    global minimum
+    global n
+
+    if depth == n:
+        maximum = max(maximum, result)
+        minimum = min(minimum, result)
+        return
+    if p > 0:
+        result += arr[depth]
+        dfs(p-1, m, mul, d, result, arr, depth+1)
+    if m > 0:
+        result -= arr[depth]
+        dfs(p, m-1, mul, d, result, arr, depth+1)
+    if mul > 0:
+        result *= arr[depth]
+        dfs(p, m, mul-1, d, result, arr, depth+1)
+    if d > 0:
+        result //= arr[depth]
+        dfs(p, m, mul, d-1, result, arr, depth+1)
+
+
+n = int(input())
+arr = list(map(int, input().split()))
+plus, minus, mul, div = map(int, input().split())
+
+dfs(plus, minus, mul, div, arr[0], arr, 1)
+print(maximum)
+print(minimum)

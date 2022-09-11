8db92b7b7d1421c7afe69e16e33195146e7bfc22
Moony-H <moonylight247@gmail.com>
2022-09-11 23:58:39 +0900

    1759 password making

--- Python/test.py
+++ Python/test.py
@@ -1,33 +1,33 @@
-cache=[[int(1e9) for _ in range(152)]for _ in range(152)]
+import sys
 
+input=sys.stdin.readline
 
-def dfs(cost,problems,ability,goal):
-    global cache
-    if ability[0]>=goal[0] and ability[1]>=goal[1]:
-        return
+answers=[]
+
     
-    for i in problems:
-        if i[0]<=ability[0] and i[1]<=ability[1]:
-            if(ability[0]+i[2]>=152) or (ability[1]+i[3]>=152):
-                continue
-            if(cache[ability[0]+i[2]][ability[1]+i[3]]<=cost+i[4]):
-                continue
-            
-            else:
-                cache[ability[0]+i[2]][ability[1]+i[3]]=cost+i[4]
-                dfs(cost+i[4],problems,(ability[0]+i[2],ability[1]+i[3]),goal)
 
+L,_=map(int,input().rstrip().split())
 
+alpha=input().rstrip().split()
 
-def solution(alp, cop, problems):
-    answer = 0
-    global cache
-    goal=[max([i[0] for i in problems]),max([i[1] for i in problems])]
-    problems.append([0,0,1,0,1])
-    problems.append([0,0,0,1,1])
-    
-    dfs(0,problems,(alp,cop),goal)
-    print(cache[goal[0]][goal[1]])
+alpha.sort()
+def aeiou(s):
+    answer=0
+    for i in ['a','e','i','o','u']:
+        answer+=s.count(i)
     return answer
+def dfs(s,depth,index,array,length):
+    global answers
+    if(depth==0 and len(s)==length):
+        num=aeiou(s)
+        if(num>=1 and len(s)-num>=2):
+            answers.append(s)
+    for i in range(index,len(array)):
+        dfs(s,depth-1,i+1,array,length)
+        dfs(s+array[i],depth-1,i+1,array,length)
 
-solution(10,10,[[10,15,2,1,2],[20,20,3,3,4]])
+dfs('',L,0,alpha,L)
+for i in answers:
+    print(i)
+
+

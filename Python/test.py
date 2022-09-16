34007742e7e53c13fa99ad6f8427588020a91ab8
Moony-H <moonylight247@gmail.com>
2022-09-16 23:56:21 +0900

    2048

--- Python/test.py
+++ Python/test.py
@@ -4,30 +4,64 @@ import sys
 input=sys.stdin.readline
 
 n=int(input().rstrip())
-
-arr=list(map(int,input().rstrip().split()))
+table=[]
+for _ in range(n):
+    table.append(list(map(int,input().rstrip().split())))
 
 answer=0
 
-def calc(arr):
-    result=0
-    for i in range(len(arr)-1):
-        result+=abs(arr[i]-arr[i+1])
-    return result
+def move(table,dir):
+    temp=[[i for i in j]for j in table]
+    if(dir==0):
+        #왼쪽
+        for i in range(len(temp)):
+            for j in range(len(temp)-1):
+                if temp[i][j]!=temp[i][j+1] and temp[i][j]!=0:
+                    continue
+                temp[i][j]+=temp[i][j+1]
+                temp[i][j+1]=0
+    if(dir==1):
+        #오른쪽
+        for i in range(len(temp)):
+            for j in range(len(temp)-1,0,-1):
+                if temp[i][j]!=temp[i][j-1] and temp[i][j]!=0:
+                    continue
+                temp[i][j]+=temp[i][j-1]
+                temp[i][j-1]=0
+    if(dir==2):
+        #위
+        for i in range(len(temp)):
+            for j in range(len(temp)-1):
+                if temp[j][i]!=temp[j+1][i] and temp[j][i]!=0:
+                    continue
+                temp[j][i]+=temp[j+1][i]
+                temp[j+1][i]=0
+    if(dir==3):
+        #아래
+        for i in range(len(temp)):
+            for j in range(len(temp)-1,0,-1):
+                if temp[j][i]!=temp[j-1][i] and temp[j][i]!=0:
+                    continue
+                temp[j][i]+=temp[j-1][i]
+                temp[j-1][i]=0
+    return temp
 
-def pick(choice,picked,arr):
+
+def find(table,num):
+
     global answer
-    if(len(picked)==len(arr)):
-        answer=max(answer,calc(picked))
-    for i in range(len(arr)):
-        if i in choice:
-            continue
-        choice.append(i)
-        picked.append(arr[i])
-        pick(choice,picked,arr)
-        choice.pop()
-        picked.pop()
+    if num>=5:
+        maximum=0
+        for i in table:
+            for j in i:
+                maximum=max(maximum,j)
+        answer=max(answer,maximum)
+        return
+    for i in range(4):
+        find(move(table,i),num+1)
 
-pick([],[],arr)
 
+
+
+find(table,1)
 print(answer)

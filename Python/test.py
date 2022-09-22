bfcf445c67203348a4c64ccee43ba2223daca12f
Moony-H <moonylight247@gmail.com>
2022-09-22 23:59:14 +0900

    mask

--- Python/test.py
+++ Python/test.py
@@ -1,67 +1,37 @@
 
 import sys
+from collections import deque
 
 input=sys.stdin.readline
 
 n=int(input().rstrip())
-table=[]
-for _ in range(n):
-    table.append(list(map(int,input().rstrip().split())))
+mask=0
+for i in range(n):
+    cmd,num=input().rstrip().split()
+    num=int(num)
+    if(cmd=='add'):
+        mask=mask&1<<num
 
-answer=0
+    elif cmd=='remove':
+        mask=mask&~(1<<num)
+    elif cmd=='check':
+        if((mask&1<<num)):
+            print(1)
+        else:
+            print(0)
 
-def move(table,dir):
-    temp=[[i for i in j]for j in table]
-    if(dir==0):
-        #왼쪽
-        for i in range(len(temp)):
-            for j in range(len(temp)-1,0,-1):
-                if temp[i][j]!=temp[i][j-1] and temp[i][j-1]!=0:
-                    continue
-                temp[i][j-1]+=temp[i][j]
-                temp[i][j-1]=0
-    if(dir==1):
-        #오른쪽
-        for i in range(len(temp)):
-            for j in range(len(temp)-1):
-                if temp[i][j]!=temp[i][j+1] and temp[i][j+1]!=0:
-                    continue
-                temp[i][j+1]+=temp[i][j]
-                temp[i][j]=0
-    if(dir==2):
-        #위
-        for i in range(len(temp)):
-            for j in range(len(temp)-1,0,-1):
-                if temp[j][i]!=temp[j-1][i] and temp[j-1][i]!=0:
-                    continue
-                temp[j-1][i]+=temp[j][i]
-                temp[j][i]=0
-    if(dir==3):
-        #아래
-        for i in range(len(temp)):
-            for j in range(len(temp)-1):
-                if temp[j][i]!=temp[j+1][i] and temp[j+1][i]!=0:
-                    continue
-                temp[j+1][i]+=temp[j][i]
-                temp[j][i]=0
-    return temp
+    elif cmd=='toggle':
+        if(mask&1<<num):
+            mask=mask&~(1<<num)
+        else:
+            mask=mask&1<<num
 
+    elif cmd=='all':
+        mask=0
+        for j in range(1,21):
+            mask+=1<<j
+    else:
+        mask=0
 
-def find(table,num):
+        
 
-    global answer
-    if num>=5:
-        maximum=0
-        for i in table:
-            for j in i:
-                maximum=max(maximum,j)
-        answer=max(answer,maximum)
-        return
-    for i in range(4):
-        find(move(table,i),num+1)
-
-
-
-
-find(table,1)
-print(answer)

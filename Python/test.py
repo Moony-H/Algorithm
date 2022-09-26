363f846819707c39023ed9f3afcfbdb2fcf0a7f5
Moony-H <moonylight247@gmail.com>
2022-09-26 21:24:23 +0900

    multi

--- Python/test.py
+++ Python/test.py
@@ -1,38 +1,70 @@
 
-import sys
+from collections import deque
 
-input=sys.stdin.readline
+def bfs(table,red,blue):
+    q=deque()
+    q.append((red[0], red[1],blue[0],blue[1], 0))
 
-n=int(input().rstrip())
-mask=0
+    UDLR=[[0,-1],[0,1],[-1,0],[1,0]]
+    while q:
+        rx,ry,bx,by,num=q.popleft()
+        if num>10:
+            return -1
+        if table[by][bx]=="O":
+            continue
+        if table[ry][rx]=='O':
+            return num
+        for i in range(len(UDLR)):
+            dbx=bx+UDLR[i][0]
+            dby=by+UDLR[i][1]
+            drx=rx+UDLR[i][0]
+            dry=ry+UDLR[i][1]
+            if table[dby][dbx]=='#':
+                dbx=bx
+                dby=by
+            if table[dry][drx]=='#':
+                drx=rx
+                dry=ry
+            
+            if drx==dbx and dby==dry:
+                if i==0:
+                    if ry>by:
+                        dry=ry
+                    else:
+                        dby=by
+                if i==1:
+                    if ry>by:
+                        dby=by
+                    else:
+                        dry=ry
+                if i==2:
+                    if bx>rx:
+                        dbx=bx
+                    else:
+                        drx=rx
+                else:
+                    if bx>rx:
+                        drx=rx
+                    else:
+                        dbx=bx
+            q.append((drx,dry,dbx,dby,num+1))
+
+
+n,m=map(int,input().rstrip().split())
+table=[]
+for _ in range(n):
+    table.append(list(input()))
+
+startR=()
+startB=()
 for i in range(n):
-    temp=list(input().rstrip().split())
-    if len(temp)==1:
-        if temp[0]=='all':
-            mask=0b111111111111111111111
-            print(mask)
-        else:
-            mask=0
-    else:
-        num=int(temp[1])
-        cmd=temp[0]
-        if(cmd=='add'):
-            mask=mask&1<<num
-
-        elif cmd=='remove':
-            mask=mask&~(1<<num)
-        elif cmd=='check':
-            if((mask&1<<num)):
-                print(1)
-            else:
-                print(0)
-
-        elif cmd=='toggle':
-            if(mask&1<<num):
-                mask=mask&~(1<<num)
-            else:
-                mask=mask&1<<num
-
-   
+    for j in range(m):
+        if table[i][j]=='R':
+            startR=(j,i)
+            table[i][j]='.'
+        if table[i][j]=='B':
+            startB=(j,i)
+            table[i][j]='.'
+print(bfs(table,startR,startB))
 
 

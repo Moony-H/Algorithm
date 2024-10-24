08062a5de7a512df3b4a8c5ef22e8375885861ea
Moony-H <53536205+Moony-H@users.noreply.github.com>
2024-10-24 23:58:39 +0900

    폰켓못 c++

---
+++ C++/프로그래머스/폰켓몬.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    unordered_map<int,int> map;
+    int answer = 0;
+    
+    for(int i=0; i<nums.size();i++)
+    {
+        auto temp=nums[i];
+        map[temp]=map[temp]+1;
+    }
+    
+    int a=nums.size()/2;
+    for (const auto& pair : map) {
+        answer+=1;
+    }
+    
+    if(answer>a){
+        return a;
+    }
+    return answer;
+}

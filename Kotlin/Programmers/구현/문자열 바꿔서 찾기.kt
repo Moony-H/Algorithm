4fdbe1fccbbc27c5ae57fcc2db8aa9c0f1a8d47f
Moony-H <53536205+Moony-H@users.noreply.github.com>
2025-02-13 18:51:55 +0900

    문자열 바꿔서 찾기

---
+++ Kotlin/Programmers/구현/문자열 바꿔서 찾기.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(myString: String, pat: String) =
+        if(pat in myString.map{if(it=='A') "B" else "A"}.joinToString(""))
+            1
+        else
+            0
+}

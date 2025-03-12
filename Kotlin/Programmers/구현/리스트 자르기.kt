6729c474d82f66beb8b9b60f111b6e5c7c187b7f
Moony-H <53536205+Moony-H@users.noreply.github.com>
2025-03-12 11:55:07 +0900

    리스트 자르기 수정

--- Kotlin/Programmers/구현/리스트 자르기.kt
+++ Kotlin/Programmers/구현/리스트 자르기.kt
@@ -1,9 +1,9 @@
 class Solution {
     fun solution(n: Int, s: IntArray, l: IntArray) =
     when(n){
-        1-> l.slice(0..s[1]).toList()
-        2-> l.slice(s[0] until s.size).toList()
-        3-> l.slice(s[0]..s[1]).toList()
-        else-> l.slice(s[0]..s[1]).filterIndexed{i,e-> (i+1)%s[2]!=0}
+        1-> l.sliceArray(0..s[1])
+        2-> l.sliceArray(s[0] until l.size)
+        3-> l.sliceArray(s[0]..s[1])
+        else-> l.slice(s[0]..s[1] step s[2])
     }
 }
